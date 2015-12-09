55f1c51cc320d97d85025a9ac977a9bdc66241a8
Pierre-Chanel <pierrechanel.gauthier@gmail.com>
2015-12-09 20:49:45 +0100

    Day 7 (Part one)

--- day7.php
+++ day7.php
@@ -5,7 +5,6 @@ $instructions = explode(PHP_EOL, file_get_contents('inputs/day7'), -1);
 // Part one
 $circuit = [];
 execRequired('a', $circuit, $instructions);
-var_dump($circuit);
 echo 'Part one: '.$circuit['a'];
 echo '
 ';
@@ -15,36 +14,40 @@ echo '
 ';
 
 function execRequired($required, &$circuit, &$instructions) {
-    $instruction = findWireInstruction($required, $instructions);
-    if ($instruction) {
-        foreach (getRequired($instruction) as $required) {
+    $result = findWireInstruction($required, $instructions);
+    if (!empty($result)) {
+        $instruction = $result['instruction'];
+        foreach (getRequired($instruction, $circuit) as $required) {
             if (!isset($circuit[$required])) {
                 execRequired($required, $circuit, $instructions);
             }
         }
         execInstruction($instruction, $circuit);
+        echo $instruction.'
+';
+        unset($instructions[$result['key']]);
     }
 }
 
-function getRequired($instruction) {
+function getRequired($instruction, &$circuit) {
     $exploded = explode(' ', $instruction);
     $required = [];
     switch (count($exploded)) {
         case 3:
-            if (!ctype_digit($exploded[0])) {
+            if (!ctype_digit($exploded[0]) && !isset($circuit[$exploded[0]])) {
                 $required[] = $exploded[0];
             }
             break;
         case 4: // NOT
-            if (!ctype_digit($exploded[1])) {
+            if (!ctype_digit($exploded[1]) && !isset($circuit[$exploded[1]])) {
                 $required[] = $exploded[1];
             }
             break;
         case 5:
-            if (!ctype_digit($exploded[0])) {
+            if (!ctype_digit($exploded[0]) && !isset($circuit[$exploded[0]])) {
                 $required[] = $exploded[0];
             }
-            if (!ctype_digit($exploded[2])) {
+            if (!ctype_digit($exploded[2]) && !isset($circuit[$exploded[2]])) {
                 $required[] = $exploded[2];
             }
             break;
@@ -53,14 +56,17 @@ function getRequired($instruction) {
 }
 
 function findWireInstruction($required, &$instructions) {
-    foreach ($instructions as $instruction) {
+    $return = [];
+    foreach ($instructions as $key => $instruction) {
         $exploded = explode(' ', $instruction);
         $wire = end($exploded);
         if ($wire == $required) {
-            return $instruction;
+            $return['instruction'] = $instruction;
+            $return['key'] = $key;
+            break;
         }
     }
-    return false;
+    return $return;
 }
 
 function execInstruction($instruction, &$circuit) {
@@ -71,41 +77,34 @@ function execInstruction($instruction, &$circuit) {
             $signal = getSignal($exploded[0], $circuit);
             break;
         case 4: // NOT
-            $signal = getSignal($exploded[1], $circuit);
-            $signal = $signal ? ((~ $signal) & 65535) : false;
+            $signal = ((~ getSignal($exploded[1], $circuit)) & 65535);
             break;
         case 5:
             $signal1 = getSignal($exploded[0], $circuit);
             $signal2 = getSignal($exploded[2], $circuit);
             switch ($exploded[1]) {
                 case 'AND':
-                    $signal = ($signal1 && $signal2) ? ($signal1 & $signal2) : false;
+                    $signal = $signal1 & $signal2;
                     break;
                 case 'OR':
-                    $signal = ($signal1 && $signal2) ? ($signal1 | $signal2) : false;
+                    $signal = $signal1 | $signal2;
                     break;
                 case 'LSHIFT':
-                    $signal = ($signal1 && $signal2) ? ($signal1 << $signal2) : false;
+                    $signal = $signal1 << $signal2;
                     break;
                 case 'RSHIFT':
-                    $signal = ($signal1 && $signal2) ? ($signal1 >> $signal2) : false;
+                    $signal = $signal1 >> $signal2;
                     break;
             }
             break;
-        default:
-            $signal = false;
-            break;
     }
-    if ($signal) {
-        $circuit[$wire] = $signal;
-    }
+    $circuit[$wire] = $signal;
 }
 
 function getSignal($input, &$circuit) {
     if (ctype_digit($input)) {
         return (int) $input;
-    } elseif (isset($circuit[$input])) {
+    } else {
         return $circuit[$input];
     }
-    return false;
 }
