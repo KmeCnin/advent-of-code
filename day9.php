1728190a7a25142ced4a5687690f33fba3924e4b
Pierre-Chanel <pierrechanel.gauthier@gmail.com>
2015-12-14 14:00:52 +0100

    Day 9

---
+++ day9.php
@@ -0,0 +1,41 @@
+<?php
+
+$input = explode(PHP_EOL, file_get_contents('inputs/day9'), -1);
+
+// Part one
+$map = [];
+foreach ($input as $data) {
+    $exploded = explode(' ', $data);
+    $map[$exploded[0]][$exploded[2]] = $exploded[4];
+    $map[$exploded[2]][$exploded[0]] = $exploded[4];
+}
+$towns = array_keys($map);
+
+findPaths($towns, $map, $paths);
+
+echo 'Part one: '.max($paths);
+echo '
+';
+
+function findPaths($items, $map, &$paths, $perms = []) {
+    if (!empty($items)) {
+        $count = count($items); 
+        for ($i = $count - 1; $i >= 0; --$i) {
+             $newitems = $items;
+             $newperms = $perms;
+             list($foo) = array_splice($newitems, $i, 1);
+             array_unshift($newperms, $foo);
+             findPaths($newitems, $map, $paths, $newperms);
+         }
+    } else {
+        $dist = 0;
+        foreach ($perms as $key => $perm) {
+            if (isset($perms[$key+1])) {
+                $next = $perms[$key+1];
+                $dist += $map[$perm][$next];
+            }
+        }
+        $paths[] = $dist;
+    }
+}
+
