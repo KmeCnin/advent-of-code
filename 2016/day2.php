e039af49938b5a5d962f450c9bfaf01f151c7b5a
Pierre-Chanel <pierrechanel.gauthier@gmail.com>
2016-12-07 12:24:47 +0100

    Add part2 to Day2

--- 2016/day2.php
+++ 2016/day2.php
@@ -33,4 +33,47 @@ foreach ($digitsInstructions as $digitInstructions) {
 }
 
 echo "Part1: $digicode\n";
-echo "Part2: \n";
+
+$keypad = [
+/*     0    1    2    3    4     5   6*/
+/*0*/[null,null,null,null,null,null,null],
+/*1*/[null,null,null,'1',null, null,null],
+/*2*/[null,null,'2', '3', '4', null,null],
+/*3*/[null,'5', '6', '7', '8',  '9',null],
+/*4*/[null,null,'A', 'B', 'C', null,null],
+/*5*/[null,null,null,'D',null, null,null],
+/*6*/[null,null,null,null,null,null,null],
+];
+$position = [1, 3];
+$digicode = '';
+foreach ($digitsInstructions as $digitInstructions) {
+    $instructions = str_split($digitInstructions);
+    foreach ($instructions as $instruction) {
+        switch ($instruction) {
+            case 'U':
+                if ($keypad[$position[0]-1][$position[1]]) {
+                    $position[0]--;
+                }
+                break;
+            case 'R':
+                if ($keypad[$position[0]][$position[1]+1]) {
+                    $position[1]++;
+                }
+                break;
+            case 'D':
+                if ($keypad[$position[0]+1][$position[1]]) {
+                    $position[0]++;
+                }
+                break;
+            case 'L':
+                if ($keypad[$position[0]][$position[1]-1]) {
+                    $position[1]--;
+                }
+                break;
+        }
+        //var_dump($position);
+    }
+    $digicode .= $keypad[$position[0]][$position[1]];
+}
+
+echo "Part2: $digicode\n";
