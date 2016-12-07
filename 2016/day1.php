e783bfc651b02856ede9c90dc5bfb861bda5d2cb
Pierre-Chanel <pierrechanel.gauthier@gmail.com>
2016-12-07 11:43:23 +0100

    Fix day1 part 2

--- 2016/day1.php
+++ 2016/day1.php
@@ -1,7 +1,6 @@
 <?php
 
 $day1 = file_get_contents('inputs/day1');
-$day1 = 'R8, R4, R4, R8';
 $input = explode(', ', $day1);
 
 //           X  Y
@@ -21,27 +20,13 @@ foreach ($input as $instruction) {
             break;
     }
     for($i=0; $i<$step; $i++) {
-        switch ($facing) {
-            case 0:
-                $position[0]++;
-                break;
-            case 1:
-                $position[1]++;
-                break;
-            case 2:
-                $position[0]--;
-                break;
-            case 3:
-                $position[1]--;
-                break;
-        }
-        if (isset($visited[$position[0]]) && 
-            $visited[$position[0]] === $position[1] &&
+        $position[$facing%2===0] += $facing<=1 ? 1 : -1;
+        if (isset($visited[$position[0].'-'.$position[1]]) &&
             null === $twice
         ) {
-            $twice = array_sum(array_map('abs', $position));
+            $twice = $visited[$position[0].'-'.$position[1]];
         } elseif (null === $twice) {
-            $visited[$position[0]] = $position[1];
+            $visited[$position[0].'-'.$position[1]] = array_sum(array_map('abs', $position));
         }
     }
 }
