2e11892c4b1eb1ba884b0a73bdac3ece12e409ef
Pierre-Chanel Gauthier <pierrechanel.gauthier@gmail.com>
2018-12-02 18:32:11 +0100

    Improve using arnaud-lb solution

--- 2018/day1/puzzle1.hs
+++ 2018/day1/puzzle1.hs
@@ -1,13 +1,9 @@
 module Main where
-    import Data.List.Split
-    import Data.List
-    
-    -- Extract sign from number strings.
-    sign string = take 1 string 
-    -- Remove + from positiv integers.
-    cleanStrInt string = if sign string == "+" then drop 1 string else string
-    -- Cast cleaned number strings to integers.
-    strToInt string = read string :: Integer
 
-    main = do input <- readFile "input"
-              print $ foldl (+) 0 $ map strToInt $ map cleanStrInt $ splitOn "\n" input
+    castInt :: [Char] -> Int
+    castInt ('+':xs) = castInt xs
+    castInt string = read string
+
+    main = do
+        input <- readFile "input"
+        print $ foldl (+) 0 $ map castInt $ lines input
