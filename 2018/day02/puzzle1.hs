9a9e9187d672df2332927f805780ca14469b96ae
Pierre-Chanel Gauthier <pierrechanel.gauthier@gmail.com>
2018-12-06 22:47:22 +0100

    Day 2 - Puzzle 1

--- 2018/day02/puzzle1.hs
+++ 2018/day02/puzzle1.hs
@@ -6,13 +6,13 @@ truthTuple list = (elem 2 list, elem 3 list)
 -- Return a tuple where first element is either there is twins letters and
 -- the second element is either there is triplets
 generateSignature :: String -> (Bool, Bool)
-generateSignature = truthTuple . nub . map (length) . group . sort
+generateSignature = truthTuple . nub . map length . group . sort
 
 checksum :: ([Bool], [Bool]) -> Int
-checksum tuple = foo * bar
+checksum tuple = twins * triplets
     where
-        foo = sum . map fromEnum . fst $ tuple
-        bar = sum . map fromEnum . snd $ tuple
+        twins    = sum . map fromEnum . fst $ tuple
+        triplets = sum . map fromEnum . snd $ tuple
 
 
 main :: IO ()
