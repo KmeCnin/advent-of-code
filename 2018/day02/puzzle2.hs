64579dec30831683a02ec63a1cccb065eb164334
Pierre-Chanel Gauthier <pierrechanel.gauthier@gmail.com>
2018-12-07 23:57:28 +0100

    Shorten code

--- 2018/day02/puzzle2.hs
+++ 2018/day02/puzzle2.hs
@@ -5,18 +5,9 @@ import Data.Maybe
 diff :: String -> String -> Bool
 diff str1 str2 = 1 == (sum . map fromEnum $ zipWith (/=) str1 str2)
 
--- Try to find the matching box from a list
-match :: String -> [String] -> Maybe String
-match needle haystack = find (diff needle) haystack
-
--- Iterate over all boxes to find the both that match
+-- Iterate over all boxes to find both that match
 boxesId :: [String] -> String
-boxesId (needle:haystack) = 
-    if isJust matched
-    then needle `intersect` fromJust matched
-    else boxesId haystack
-    where
-        matched = match needle haystack
+boxesId (needle:haystack) = fromMaybe (boxesId haystack) . find (diff needle) $ haystack
 
 main :: IO ()
 main = print . boxesId . lines =<< readFile "input"
