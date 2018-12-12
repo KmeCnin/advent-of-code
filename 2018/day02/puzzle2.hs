2e644ccc228951486c5b879a163b158a385ba14f
Pierre-Chanel Gauthier <pierrechanel.gauthier@gmail.com>
2018-12-12 13:31:39 +0100

    Improve syntaxe

--- 2018/day02/puzzle2.hs
+++ 2018/day02/puzzle2.hs
@@ -2,12 +2,12 @@ import Data.List
 import Data.Maybe
 
 -- Either both boxes have only 1 different char
-diff :: String -> String -> Bool
-diff str1 str2 = 1 == (sum . map fromEnum $ zipWith (/=) str1 str2)
+match :: String -> String -> Bool
+match str1 str2 = (== 1) . sum . map fromEnum $ zipWith (/=) str1 str2
 
 -- Iterate over all boxes to find both that match
 boxesId :: [String] -> String
-boxesId (needle:haystack) = fromMaybe (boxesId haystack) . find (diff needle) $ haystack
+boxesId (needle:haystack) = fromMaybe (boxesId haystack) . find (match needle) $ haystack
 
 main :: IO ()
 main = print . boxesId . lines =<< readFile "input"
